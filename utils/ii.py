2049da22870f517dd6f452d92cb23ed59c2e6881
Tianzi Luo <tluo023@ucr.edu>
2025-07-01 11:15:25 -0700

    add month check feature

--- utils/ii.py
+++ utils/ii.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 from utils.copy_file import latest_matching_file, copy_file
+from datetime import datetime
 
 def copy_weChat_files():
-    source = Path(r"C:\Users\monica\Documents\xwechat_files\qingchen536521_c584\msg\file\2025-06")
+    month = datetime.now().strftime("%Y-%m")
+    source = Path(fr"C:\Users\monica\Documents\xwechat_files\qingchen536521_c584\msg\file\{month}")
     target = Path(r"C:\Frank\易仓-TP\无小票发货 Sarah")
     keywords = ["新范本", "店小秘 非BW", "店小秘 BW"]
 
@@ -13,7 +15,9 @@ def copy_weChat_files():
         else:
             print(f"⚠️ 未找到包含「{kw}」的 Excel 文件")
 
+
 '''
 if __name__ == "__main__":
     copy_weChat_files()
+    print
 '''
