f439b5976fcdae1c0c37523acc4eefbfc434cbfc
Tianzi Luo <tluo023@ucr.edu>
2025-09-05 11:53:13 -0700

    add amazon ops

---
+++ go.mod
@@ -0,0 +1,3 @@
+module amzfileops
+
+go 1.24.5
--- utils/i.py
+++ utils/i.py
@@ -3,18 +3,14 @@ import time
 from pathlib import Path
 from datetime import datetime
 from send2trash import send2trash
-from utils.copy_file import latest_matching_file, copy_file 
-
+from utils.copy_file import latest_matching_file, copy_file
 import shutil
-from pathlib import Path
 
 def clean_folder(folder_path: Path, target_folder: Path):
     file_patterns = ['*.xlsx', '*.csv']
     files_to_move = []
-
     for pattern in file_patterns:
         files_to_move.extend(folder_path.glob(pattern))
-
     for file in files_to_move:
         try:
             destination = target_folder / file.name
@@ -26,7 +22,6 @@ def clean_folder(folder_path: Path, target_folder: Path):
 def copy_wechat_files(target: Path, keywords: list[str]):
     month = datetime.now().strftime("%Y-%m")
     source = Path(fr"C:\Users\monica\Documents\xwechat_files\qingchen536521_c584\msg\file\{month}")
-
     for kw in keywords:
         latest = latest_matching_file(source, [".xlsx"], [kw], only_today=False)
         if latest:
@@ -39,11 +34,11 @@ def clean_folder_and_copy_files():
     # BW DV
     target = Path(r"C:\ACT\公用核心\每天自发货")
     folder = Path(r"C:\ACT\公用核心\每天自发货\历史文件")
-    keywords = ["发货小票", "店小秘 非BW","TP", "店小秘 BW"]
-    clean_folder(target,folder)
+    keywords = ["发货小票", "店小秘 非BW", "TP", "店小秘 BW"]
+
+    clean_folder(target, folder)
     time.sleep(1)
     copy_wechat_files(target, keywords)
-
     os.startfile(target)
 
 '''
